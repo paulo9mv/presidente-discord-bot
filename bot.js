595a86499d7eefa83ed1cc95439bda6c6bb5ec12
Paulo Viana <paulo9mv@yahoo.com.br>
2021-06-18 23:02:14 -0400

    feat: adjust secret

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //Modules
 const Discord = require('discord.js');
-const rp = require('request-promise');
-const $ = require('cheerio');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 
 //Client
 const client = new Discord.Client();
@@ -10,6 +10,8 @@ let generated = 0
 
 //Imports
 const presidentes = require('./presidentes.json');
+const token = require('./secret.json');
+
 const strings = require('./strings/strings.json');
 
 client.on('ready', () => {
@@ -17,7 +19,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.on('message', msg => {
+client.on('message', async msg => {
 
   if (msg.author.bot)
     return;
@@ -37,88 +39,24 @@ client.on('message', msg => {
     return;
   }
 
-  if (msg.content == strings.random_cmd) {
+  if (msg.content == '.') {
 
     let rand = Math.floor(Math.random() * presidentes.name.length);
     let nome = presidentes.name[rand];
 
-    let parseNome = nome.replace(/\s/g, "_");
+    let parseNome = nome.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/\s/g, "_");
     console.log('nome', nome)
 
-    rp(strings.url + parseNome)
-      .then(function (html) {
-        let frases = [];
-        let frase = $('.mw-parser-output', html);
+    const url = strings.url + parseNome
 
-        frase = frase[0];
+    const response = await fetch(url);
+    const body = await response.text();
 
-        for (let i = 0; i < frase.children.length; i++) {
+    const $ = cheerio.load(body)
+    const bodyContent = $('#bodyContent');
+    console.log(bodyContent)
 
-          if (frase.children[i].attribs != undefined) {
-            let classe = frase.children[i].attribs.name;
-            let tag = frase.children[i].name;
-
-            if (classe == "noprint")
-              break;
-            if (tag == "h2") {
-              let atributos = frase.children[i].children[0].attribs;
-
-              if (atributos != undefined)
-                if (atributos.id == "Sobre")
-                  break;
-            }
-          }
-
-          if (frase.children[i].name == "ul") {
-            let tmp = frase.children[i];
-
-            if (tmp.children[0].name == "li") {
-              let str = "";
-              let tag_li = tmp.children[0];
-              let tag_li_children_length = tag_li.children.length;
-
-              for (let j = 0; j < tag_li_children_length; j++) {
-                let current_tag = tag_li.children[j];
-                if (current_tag.children == undefined) {
-                  let frase = current_tag.data;
-                  str = str.concat(frase);
-                }
-                else {
-                  let current_tag_children_length = current_tag.children.length;
-                  if (current_tag_children_length > 0)
-                    for (let k = 0; k < current_tag_children_length; k++) {
-                      let frase = current_tag.children[k].data;
-                      str = str.concat(frase);
-                    }
-                }
-              }
-              str = str.concat('\n-' + nome);
-              frases.push(str);
-              str = "";
-            }
-          }
-        }
-
-        let photosrc = $('div > a > img', html).attr("src");
-        let photourl = 'http:' + photosrc
-
-        let indexFrase = Math.floor(Math.random() * frases.length);
-        let message = frases[indexFrase];
-
-        //Send photo
-        msg.channel.send({
-          files: [photourl]
-        })
-          .then(function () {
-            generated++;
-            msg.channel.send(message)  //After photo sent, sent quote
-          })
-          .catch(console.error);
-      }).catch(e => {
-        msg.channel.send('A oposição não me permite trabalhar. Repita o comando, por favor.')
-        console.log(strings.url + parseNome)
-      })
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN || token.token);
