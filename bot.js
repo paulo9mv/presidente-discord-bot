0f3cab6c277f61a5cdebda8a7b99920b188ad055
Paulo Viana <paulo9mv@yahoo.com.br>
2021-06-19 03:43:17 -0400

    feat: envia corretamentes frases do pensador.com

--- bot.js
+++ bot.js
@@ -11,13 +11,17 @@ let generated = 0
 //Imports
 const presidentes = require('./presidentes.json');
 const token = require('./secret.json');
-
 const strings = require('./strings/strings.json');
 
 client.on('ready', () => {
   client.user.setActivity('Hino Nacional Brasileiro', { type: 'LISTENING' });
   console.log(`Logged in as ${client.user.tag}!`);
 })
+
+var randomProperty = function (obj) {
+  var keys = Object.keys(obj);
+  return obj[keys[keys.length * Math.random() << 0]];
+};
 
 client.on('message', async msg => {
 
@@ -40,21 +44,55 @@ client.on('message', async msg => {
   }
 
   if (msg.content == '.') {
+    const safePresidentes = JSON.parse(JSON.stringify(presidentes))
+    const parsePresidentes = (obj) => {
+      const keys = Object.keys(obj)
 
-    let rand = Math.floor(Math.random() * presidentes.name.length);
-    let nome = presidentes.name[rand];
+      for (key of keys) {
+        console.log(key)
 
-    let parseNome = nome.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/\s/g, "_");
-    console.log('nome', nome)
+        if (!obj[key].key) {
+          delete obj[key]
+        }
+      }
 
-    const url = strings.url + parseNome
+      console.log(obj)
+    }
 
-    const response = await fetch(url);
+    parsePresidentes(safePresidentes)
+    const randomPresident = randomProperty(safePresidentes)
+
+    const nome = randomPresident.name;
+    const presidentKey = randomPresident.key
+
+    const response = await fetch(`https://www.pensador.com/author/${presidentKey}/`);
     const body = await response.text();
 
     const $ = cheerio.load(body)
-    const bodyContent = $('#bodyContent');
-    console.log(bodyContent)
+    const frases = []
+
+    $('.frase.fr').each((index, item) => {
+      const children = item.children
+
+      children && children.map(item => frases.push(item.data))
+
+    })
+
+    $('.frase.fr0').each((index, item) => {
+      const children = item.children
+
+      children && children.map(item => frases.push(item.data))
+
+    })
+
+    const frase = frases[Math.floor(Math.random() * frases.length)];
+    const msg_channel = frase + '\n-' + nome
+
+    msg.channel.send(msg_channel, {
+      files: ['https://upload.wikimedia.org/wikipedia/commons/8/81/Dilma_Rousseff_-_foto_oficial_2011-01-09.jpg']
+    })
+
+
 
   }
 });
